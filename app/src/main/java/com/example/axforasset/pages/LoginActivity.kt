d33e6c3f1b286ef6f6032613ad9a328b574465c7
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-05 02:11:45 +0700

    intent and home activity

--- app/src/main/java/com/example/axforasset/pages/LoginActivity.kt
+++ app/src/main/java/com/example/axforasset/pages/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.example.axforasset.pages
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.axforasset.databinding.ActivityLoginBinding
+import com.example.axforasset.parcel.User
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,10 +16,12 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.loginBtn.setOnClickListener {
-            val uname = binding.unameEt.text.toString();
-            val pass = binding.passEt.text.toString();
+            val uname = binding.unameEt.text.toString()
+            val pass = binding.passEt.text.toString()
 
+            val User = User(uname, pass)
 
+            intent = Intent(this, )
         }
 
     }
