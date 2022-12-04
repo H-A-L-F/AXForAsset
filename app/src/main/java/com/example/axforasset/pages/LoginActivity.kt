5d0314298346b5003223babba60bee717f1e10f6
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-05 02:33:18 +0700

    blank homeActivity

--- app/src/main/java/com/example/axforasset/pages/LoginActivity.kt
+++ app/src/main/java/com/example/axforasset/pages/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.example.axforasset.pages
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.axforasset.databinding.ActivityLoginBinding
+import com.example.axforasset.parcel.User
 
 class LoginActivity : AppCompatActivity() {
 
