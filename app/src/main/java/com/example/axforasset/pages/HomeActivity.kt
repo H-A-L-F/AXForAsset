d1aa799ce17c8723542a82b393e0168bfe856216
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-05 02:35:28 +0700

    fixing homeActivity

--- app/src/main/java/com/example/axforasset/pages/HomeActivity.kt
+++ app/src/main/java/com/example/axforasset/pages/HomeActivity.kt
@@ -2,10 +2,15 @@ package com.example.axforasset.pages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.axforasset.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home2)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
