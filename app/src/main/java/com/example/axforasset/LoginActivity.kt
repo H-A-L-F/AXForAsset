276e500c60ef69bd974de152475bb9ef073d928f
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-05 01:59:40 +0700

    loginActivity

--- app/src/main/java/com/example/axforasset/LoginActivity.kt
+++ app/src/main/java/com/example/axforasset/LoginActivity.kt
@@ -2,10 +2,23 @@ package com.example.axforasset
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.axforasset.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            val uname = binding.unameEt.text.toString();
+            val pass = binding.passEt.text.toString();
+
+
+        }
+
     }
 }
