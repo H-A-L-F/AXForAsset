a847ad45b686d4359a8d1104888662da6a1e7a53
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-05 03:36:09 +0700

    login validation and err msg

--- app/src/main/java/com/example/axforasset/pages/LoginActivity.kt
+++ app/src/main/java/com/example/axforasset/pages/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.axforasset.pages
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.axforasset.databinding.ActivityLoginBinding
 import com.example.axforasset.parcel.User
 
@@ -19,6 +20,8 @@ class LoginActivity : AppCompatActivity() {
             val uname = binding.unameEt.text.toString();
             val pass = binding.passEt.text.toString();
 
+            if(!validateLogin(uname, pass)) return@setOnClickListener
+
             val user: User = User(uname, pass)
 
             intent = Intent(this, HomeActivity::class.java)
@@ -26,5 +29,16 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    private fun validateLogin(uname: String, pass: String): Boolean {
+        if(uname.isBlank() || pass.isBlank()) {
+            Toast.makeText(this, "Please fill all the fields!", Toast.LENGTH_SHORT).show()
+            return false
+        } else if(pass.length < 8) {
+            Toast.makeText(this, "Password length must be at least 8", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
     }
 }
