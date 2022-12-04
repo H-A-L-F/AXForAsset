fd56df01cad3eebce6ef9f48a3aaf9ece99254c4
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-05 03:14:08 +0700

    home menu and title

--- app/src/main/java/com/example/axforasset/pages/HomeActivity.kt
+++ app/src/main/java/com/example/axforasset/pages/HomeActivity.kt
@@ -2,7 +2,13 @@ package com.example.axforasset.pages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.example.axforasset.R
 import com.example.axforasset.databinding.ActivityHomeBinding
+import com.example.axforasset.parcel.User
 
 class HomeActivity : AppCompatActivity() {
 
@@ -12,5 +18,18 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val user: User = intent.getParcelableExtra("user")!!
+
+        supportActionBar?.setTitle(user.username)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return true
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return super.onContextItemSelected(item)
     }
 }
