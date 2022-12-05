f1214508bf7ded32a957396c20720445bc768958
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-05 13:09:33 +0700

    all working menu

--- app/src/main/java/com/example/axforasset/pages/ItemActivity.kt
+++ app/src/main/java/com/example/axforasset/pages/ItemActivity.kt
@@ -1,8 +1,12 @@
 
 package com.example.axforasset.pages
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.axforasset.R
 import com.example.axforasset.databinding.ActivityItemBinding
 
 class ItemActivity : AppCompatActivity() {
@@ -15,5 +19,28 @@ class ItemActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.item_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.profileMenu -> {
+                intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.homeMenu -> {
+                intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.logoutMenu -> {
+                intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/axforasset/pages/ProfileActivity.kt
+++ app/src/main/java/com/example/axforasset/pages/ProfileActivity.kt
@@ -1,7 +1,11 @@
 package com.example.axforasset.pages
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.axforasset.R
 import com.example.axforasset.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
@@ -12,5 +16,28 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.profile_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.itemMenu -> {
+                intent = Intent(this, ItemActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.homeMenu -> {
+                intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.logoutMenu -> {
+                intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
